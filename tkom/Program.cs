38cc3838a36dfba1700aac208efe19e7a5918b1c
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2021-01-10 20:24:32 +0100

    Add files via upload

--- tkom/Program.cs
+++ tkom/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tkom.LexerN;
+using tkom.ParserN;
 using System.IO;
 using System.Text;
 
@@ -11,9 +12,11 @@ namespace tkom
         {
             if(args.Length==2){
                 LexerTest ltest = new LexerTest();
+                ParserTest ptest = new ParserTest();
                 switch(args[0]){
                 case "-tlf": ltest.LexerTestFile(args[1]); break;
                 case "-tls": ltest.LexerTestString(args[1]); break;
+                case "-tpf": ptest.ParserTestFile(args[1]); break;
                 default: Console.WriteLine("Podano niepoprawną falgę. "); break;
                 }
 
