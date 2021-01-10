32dad58061387a828d803c3724da8000e8b0ef83
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2021-01-10 20:27:20 +0100

    Add files via upload

---
+++ tkom/ParserN/ParserTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using tkom.LexerN;
+using tkom.ParserN.Structures;
+
+namespace tkom.ParserN
+{
+    public class ParserTest
+    {
+        public void ParserTestFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new SourceException("Exception: There is no file with path: " + path);
+            using (var stream = new StreamReader(path))
+            {
+                Lexer leks = new Lexer(stream);
+                Parser parser = new Parser(leks);
+                ProgramStructure program = new ProgramStructure();
+                try
+                {
+                    program = parser.ParseProgram();
+                }
+                catch (ParserException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+                Console.WriteLine("Przeprasowano! :D ");
+                Console.WriteLine("Oto program: ");
+                Console.WriteLine("| "); Console.WriteLine("| ");
+                program.ConsoleWrite();
+
+            }
+        }
+    }
+}
