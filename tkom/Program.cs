cab79318ad3d809da0ebf079cef1516468bcb4e3
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2021-01-22 20:04:34 +0100

    tests

--- tkom/Program.cs
+++ tkom/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using tkom.LexerN;
 using tkom.ParserN;
+using tkom.InterpreterN;
 using System.IO;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace tkom
 {
@@ -10,23 +13,35 @@ namespace tkom
     {
         static void Main(string[] args)
         {
-            if(args.Length==2){
+            if (args.Length == 2 )
+            {
                 LexerTest ltest = new LexerTest();
                 ParserTest ptest = new ParserTest();
-                switch(args[0]){
-                case "-tlf": ltest.LexerTestFile(args[1]); break;
-                case "-tls": ltest.LexerTestString(args[1]); break;
-                case "-tpf": ptest.ParserTestFile(args[1]); break;
-                default: Console.WriteLine("Podano niepoprawną falgę. "); break;
+                InterpreterRun irun = new InterpreterRun();
+                switch (args[0])
+                {
+                    case "-tlf": 
+                        ltest.LexerTestFile(args[1]); 
+                        break;
+                    case "-tls": 
+                        ltest.LexerTestString(args[1]); 
+                        break;
+                    case "-tpf": 
+                        ptest.ParserTestFile(args[1]); 
+                        break;
+                    case "-tif": 
+                         irun.InterpreterRunStart(args[1]); 
+                        break;
+                    default: Console.WriteLine("Podano niepoprawną falgę. "); break;
                 }
-
             }
-            else{
+            else
+            {
                 Console.WriteLine("Podano niepoprawne argumenty. ");
             }
 
         }
-        
+
     }
 }
 
